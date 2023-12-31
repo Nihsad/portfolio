899460cf27e54f5ba22e3ae6f251531c3459f98b
Dashin <nihsadd@gmail.com>
2023-12-31 16:15:28 -0700

    changes

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,14 @@
 window.onscroll = function() { stickyHeader() };
 
     var header = document.getElementById("main-header");
-    var sticky = header.offsetTop;
+    var body = document.body; // Get the body element
 
     function stickyHeader() {
-      if (window.pageYOffset > sticky) {
+      if (window.pageYOffset > header.offsetTop) {
         header.classList.add("sticky");
+        body.classList.add("scrolled"); // Add the scrolled class to the body
       } else {
         header.classList.remove("sticky");
+        body.classList.remove("scrolled"); // Remove the scrolled class from the body
       }
     }
